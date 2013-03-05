3784245930aeb7cacff5ee3e86915d0603055694
mikera <mike.r.anderson.13@gmail.com>
2013-03-05 20:58:29 +0800

    Add basic cljunit testing

---
+++ src/test/clojure/mikera/orculje/test_main.clj
@@ -0,0 +1,7 @@
+(ns mikera.orculje.test-main
+  (:use [clojure test]))
+
+(deftest test1
+  (is (= 1 1)))
+  
+  
---
+++ src/test/java/mikera/orculje/test/AllClojureTests.java
@@ -0,0 +1,9 @@
+package mikera.orculje.test;
+
+
+public class AllClojureTests extends mikera.cljunit.ClojureTest {
+	@Override
+	public String filter() {
+		return "mikera.orculje";
+	}
+}
