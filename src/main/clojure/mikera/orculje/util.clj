a058773ecc7e3196092d327d02b716c7ce10687a
mikera <mike.r.anderson.13@gmail.com>
2013-03-06 14:00:41 +0800

    Removal of items from vectors

--- src/main/clojure/mikera/orculje/util.clj
+++ src/main/clojure/mikera/orculje/util.clj
@@ -1,4 +1,5 @@
-(ns mikera.orculje.util)
+(ns mikera.orculje.util
+  (:use mikera.cljutils.error))
 
 (defn find-identical-position
   "Searches a vector for an identical item and returns the index, or -1 if not found.
@@ -9,3 +10,20 @@
       (if (>= i c)
         -1
         (if (identical? item (.nth vector i)) i (recur (inc i)))))))
+
+(defn vector-without
+  "Cuts a specific position of a vector"
+  [^clojure.lang.PersistentVector vector ^long i]
+  (let [c (count vector)
+        ni (inc i)]
+    (cond 
+      (== c 1) (if (== i 0) [] (error "Idex of out range: " i))
+      (== ni c) (subvec vector 0 i)
+      :else (vec (concat (subvec vector 0 i) (subvec vector ni c))))))
+
+(defn remove-from-vector
+  "Removes a specific object from a vector. Throws an error if the object is not found."
+  [item ^clojure.lang.PersistentVector vector]
+  (let [i (find-identical-position item vector)]
+    (when (< i 0) (error "item not found!"))
+    (vector-without vector i)))
---
+++ src/test/clojure/mikera/orculje/test_util.clj
@@ -0,0 +1,11 @@
+(ns mikera.orculje.test-util
+  (:use mikera.orculje.util)
+  (:use clojure.test))
+
+(deftest test-vector-without
+  (let [a 1 b 2 c 3]
+    (= [a b] (vector-without [a b c] 2))
+    (= [a c] (vector-without [a b c] 1))
+    (= [b c] (vector-without [a b c] 0))
+    (= [] (vector-without [a] 0))))
+
