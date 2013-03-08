f2181e34b46c4ce0ab0b87381808d8c6e5cd033c
mikera <mike.r.anderson.13@gmail.com>
2013-03-08 12:23:07 +0800

    Initial demo display

--- src/main/clojure/mikera/orculje/demo/main.clj
+++ src/main/clojure/mikera/orculje/demo/main.clj
@@ -1,7 +1,27 @@
 (ns mikera.orculje.demo.main
-  (:use mikera.cljutils.error))
+  (:use mikera.cljutils.error)
+  (:import [javax.swing JFrame])
+  (:import [java.awt Font])
+  (:import [mikera.gui JConsole]))
+
+(def ^Font font (Font. "Courier New" Font/PLAIN 16))
+
+(defn new-frame 
+  (^JFrame []
+    (let [frame (JFrame.)]
+      frame)))
+
+(defn new-console
+  (^JConsole []
+    (let [jc (JConsole. 100 40)]
+      (.setMainFont jc font)
+      jc)))
 
 (defn main 
   "Main entry point to the demo, called directly from Java main() method in DemoApp"
   ([]
-    (TODO)))
+    (let [^JFrame frame (new-frame)
+          ^JConsole jc (new-console)]
+      (.add (.getContentPane frame) jc)
+      (.pack frame)
+      (.setVisible frame true))))
