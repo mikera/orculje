9c0c5acea41feb8d99900860b9931b00733893e7
mikera <mike.r.anderson.13@gmail.com>
2013-03-08 17:46:07 +0800

    Keyboard handling improvements

--- src/main/clojure/mikera/orculje/demo/main.clj
+++ src/main/clojure/mikera/orculje/demo/main.clj
@@ -1,8 +1,9 @@
 (ns mikera.orculje.demo.main
   (:use mikera.cljutils.error)
   (:use mikera.orculje.gui)
-  (:import [javax.swing JFrame  JComponent])
+  (:import [javax.swing JFrame JComponent KeyStroke])
   (:import [java.awt Font])
+  (:import [java.awt.event KeyEvent])
   (:import [mikera.gui JConsole]))
 
 (def ^Font font (Font. "Courier New" Font/PLAIN 16))
@@ -16,14 +17,42 @@
   (^JConsole []
     (let [jc (JConsole. 100 40)]
       (.setMainFont jc font)
+      (.setFocusable jc true)
       jc)))
 
+(defn make-input-handler 
+  "Builds an input handler for the specified state object"
+  ([state k]
+    (fn []
+      (println (str "Key handled: " k)))))
+
+
+(defn setup-input 
+  ([^JComponent comp state]
+    (doseq [k "abcdefghijklmnopqrstuvwxyz 01234567890!\"\\/£$%^&*()'~<>?@#_-+=[]{},."]
+      (add-input-binding comp (keystroke k) (make-input-handler state (str k))))
+    (doseq [k "ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+      (add-input-binding comp (keystroke k) (make-input-handler state (str k))))
+    (doseq [[^KeyEvent ke k] {KeyEvent/VK_LEFT "4"
+                              KeyEvent/VK_RIGHT "6"
+                              KeyEvent/VK_UP "2"
+                              KeyEvent/VK_DOWN "8"
+                              KeyEvent/VK_ESCAPE "Q"}]
+      (add-input-binding comp (keystroke-from-keyevent ke) (make-input-handler state (str k))))))
+
+(defn launch [state]
+  (let [^JFrame frame (new-frame)
+          ^JConsole jc (new-console)]
+      (setup-input jc state) 
+      (.add (.getContentPane frame) jc)
+      (.pack frame)
+      (.setVisible frame true)
+      frame))
+
+(def s {:game (atom nil)})
 
 (defn main 
   "Main entry point to the demo, called directly from Java main() method in DemoApp"
   ([]
-    (let [^JFrame frame (new-frame)
-          ^JConsole jc (new-console)]
-      (.add (.getContentPane frame) jc)
-      (.pack frame)
-      (.setVisible frame true))))
+    (let [^JFrame frame (launch s)]
+      (.setDefaultCloseOperation frame JFrame/EXIT_ON_CLOSE))))
--- src/main/clojure/mikera/orculje/gui.clj
+++ src/main/clojure/mikera/orculje/gui.clj
@@ -1,17 +1,35 @@
 (ns mikera.orculje.gui
-  (:import [java.awt.event ActionListener])
-  (:import [javax.swing JComponent KeyStroke Action]))
+  (:import [java.awt.event ActionListener KeyEvent])
+  (:import [javax.swing JComponent KeyStroke Action AbstractAction]))
 
 (defmacro action 
   "creates a java.awt.ActionListner that executes the given code"
   ([body]
-    `(reify Action
-       (actionPerformed [action event]
+    `(proxy [AbstractAction] []
+       (actionPerformed [~'event]
          ~body))))
 
 
 (defn add-input-binding [^JComponent comp ^KeyStroke keystroke f]
   (let [input-map (.getInputMap comp JComponent/WHEN_ANCESTOR_OF_FOCUSED_COMPONENT)
         action-map (.getActionMap comp)]
+    ;; (println (str "Setting up input binding: " keystroke))
     (.put input-map keystroke (str keystroke))
-    (.put action-map (str keystroke) (action (f)))))
+    (.put action-map (str keystroke) (action 
+                                       (do 
+                                         ;; (println "handled!")
+                                         (f))))))
+
+(defn keystroke 
+  "Gets a keystroke object for the given string"
+  (^KeyStroke [k]
+    (keystroke k 0))
+  (^KeyStroke [k mods]
+    (KeyStroke/getKeyStroke (.charAt (str k) 0) mods)))
+
+(defn keystroke-from-keyevent 
+  "Gets a keystroke object for the given string"
+  (^KeyStroke [^KeyEvent k]
+    (keystroke-from-keyevent k 0))
+  (^KeyStroke [^KeyEvent k mods]
+    (KeyStroke/getKeyStroke k mods)))
