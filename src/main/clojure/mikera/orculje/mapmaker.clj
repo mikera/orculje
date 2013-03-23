a8a4880358c9fb532c9a45df0e7e4e5a3dd4fecc
mikera <mike.r.anderson.13@gmail.com>
2013-03-23 11:30:23 +0800

    reduce-block mapmaker functionality

--- src/main/clojure/mikera/orculje/mapmaker.clj
+++ src/main/clojure/mikera/orculje/mapmaker.clj
@@ -21,18 +21,46 @@
           z2 (max (.z lower) (.z upper))]
       (assoc game :world (.setBlock grid x1 y1 z1 x2 y2 z2 value)))))
 
-(def PLACE_RETRIES 20)
+(defn reduce-block 
+  "Reduces over a block of squares from lmin to lmain"
+  ([f init loc1 loc2]
+    (let [lmin (loc-min loc1 loc2)
+          lmax (loc-max loc1 loc2)
+          [x1 y1 z1] lmin
+          [x2 y2 z2] lmax]
+      ;;(println (str "reduce-block range: " lmin lmax))
+      (loop [m init z (long z1)]
+        (if (<= z z2)
+          (recur 
+            (loop [m m y (long y1)]
+              (if (<= y y2)
+                (recur 
+                  (loop [m m x (long x1)]
+                    (if (<= x x2)
+                      (let [l (loc x y z)]
+                        ;;(println (str "reducing at: " l))
+                        (recur (f m l) (inc x)))
+                      m)) (inc y))
+                m)) (inc z))
+          m))))) 
 
-(defn find-loc [lmin lmax loc-pred]
-  (loop [i PLACE_RETRIES]
-    (let [l (rand-loc lmin lmax)]
-      (when (> i 0)
-        (if (loc-pred l)
-          l
-          (recur (dec i)))))))
+(def DEFAULT_PLACE_RETRIES 20)
+
+(defn find-loc 
+  "Attempts to find a location that satisfies a given predicate."
+  ([lmin lmax loc-pred]
+    (find-loc lmin lmax loc-pred DEFAULT_PLACE_RETRIES))
+  ([lmin lmax loc-pred max-retries]
+    (loop [i max-retries]
+      (let [l (rand-loc lmin lmax)]
+        (when (> i 0)
+          (if (loc-pred l)
+            l
+            (recur (dec i))))))))
 
 (defn place-thing
-  "Places a thing randomly within the given area. Returns nil if thing cannot be placed"
+  "Places a thing randomly within the given area. 
+   Returns nil if thing cannot be placed (i.e. if no unblocked square can be found)"
   [game 
    ^mikera.orculje.engine.Location la 
    ^mikera.orculje.engine.Location lb 
@@ -46,7 +74,7 @@
           y2 (max (.y lower) (.y upper))
           z1 (min (.z lower) (.z upper))
           z2 (max (.z lower) (.z upper))]
-    (loop [i PLACE_RETRIES]
+    (loop [i DEFAULT_PLACE_RETRIES]
       (when (> i 0)
         (let [x (Rand/range x1 x2)
               y (Rand/range y1 y2)
--- src/test/clojure/mikera/orculje/test_mapmaker.clj
+++ src/test/clojure/mikera/orculje/test_mapmaker.clj
@@ -8,3 +8,13 @@
         game (fill-block game (loc 0 0 0) (loc 2 2 2) {:foo true})]
     (is (:foo (get-tile game 2 2 2)))))
 
+(deftest test-reduce-block
+  (let [game (empty-game)
+        game (reduce-block 
+               (fn [game loc] (set-tile game loc :foo))
+               game (loc 0 0 0) (loc 2 2 2))]
+    (is game)
+    (is (= :foo (get-tile game (loc 0 0 0))))
+    (is (= :foo (get-tile game (loc 1 1 1))))
+    (is (= :foo (get-tile game (loc 2 2 2))))))
+
